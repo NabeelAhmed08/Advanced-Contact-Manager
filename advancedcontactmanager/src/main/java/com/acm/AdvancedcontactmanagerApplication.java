cadd85cad33d68a67f266a28570568dac819ec1a
NabeelAhmed08 <67134707+NabeelAhmed08@users.noreply.github.com>
2021-01-04 22:05:35 +0530

    added New Project

---
+++ advancedcontactmanager/src/main/java/com/acm/AdvancedcontactmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.acm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedcontactmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedcontactmanagerApplication.class, args);
+	}
+
+}
